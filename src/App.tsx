f28beadf1698916d7f746e0d009c870d1c17e466
investinginit123-a11y <investinginit123@gmail.com>
2026-01-28 15:06:52 -0600

    Wrap Home component with BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
 }
